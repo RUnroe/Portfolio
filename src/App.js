abd4931c4a5db79e5346d3c75c55a87fc7a5e254
RUnroe <ryanunroe@gmail.com>
2021-09-25 15:37:46 -0700

    Create init modules

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from '/pages/Home';
+import ProjectList from '/pages/ProjectList';
+import NavigationMenu from './partials/NavigationMenu';
+import {
+  BrowserRouter,
+  Redirect,
+  Switch,
+  Route,
+  useLocation,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <NavigationMenu />
+    
+    <div id="main">
+      <RoutesModule />
     </div>
+    <Footer/>
+  </BrowserRouter>
   );
 }
+
+function RoutesModule() {
+
+}
+
+function Footer() {
+
+}
+
+
 
 export default App;
---
+++ src/pages/ProjectList.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function ProjectList() {
+
+
+
+}
+
+
+export default ProjectList;
