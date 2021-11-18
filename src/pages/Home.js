2767af5bb8a638b8e4fd1991865220673289ad6a
RUnroe <ryanunroe@gmail.com>
2021-11-17 17:41:19 -0700

    Create base skills section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -28,12 +28,28 @@ function Home() {
             </div>
 
         </div>
-        <div className="section light-2 home-content">
-            <div className="about-me">
+        <div className="light-2 home-content">
+            <div className="section about-me">
                 <div className="container">
                     <h3>About Me</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
+            </div>
+
+            <div className="skills">
+                <div className="slanted-bg"></div>
+                <div className="container">
+                    <div className="left">
+                        <h3>Skills</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                </div>
+                <div className="right">
+                    
                 </div>
             </div>
         </div>
