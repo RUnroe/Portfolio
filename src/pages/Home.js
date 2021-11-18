23813df284d382e56ed914fa1d134dcbaa911187
RUnroe <ryanunroe@gmail.com>
2021-11-17 17:15:14 -0700

    Create about me section styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import '../styles/home.css';
 function Home() {
 
     return(
+        <>
         <div className="section gradient hero">
             <div className="container">
                 <h1>Ryan Unroe</h1>
@@ -27,6 +28,16 @@ function Home() {
             </div>
 
         </div>
+        <div className="section light-2 home-content">
+            <div className="about-me">
+                <div className="container">
+                    <h3>About Me</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+        </div>
+        </>
     )
 
 }
